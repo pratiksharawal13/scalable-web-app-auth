5c1c7295bdb101d23dfdd059dc378c226b81ace1
Pratiksha Rawal <24rawal.pratiksha@gmail.com>
2025-12-14 12:56:12 +0530

    Fix env variable usage for login API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,13 +9,16 @@ function App() {
     e.preventDefault();
 
     try {
-      const res = await fetch("process.env.REACT_APP_API_URL/api/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
+      const res = await fetch(
+        `${process.env.REACT_APP_API_URL}/api/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
 
       const data = await res.json();
 
