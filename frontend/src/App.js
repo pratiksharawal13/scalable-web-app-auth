ce64ec85bdca6ce341ac850c3b489afb100b6efa
Pratiksha Rawal <24rawal.pratiksha@gmail.com>
2025-12-14 12:34:47 +0530

    Remove localhost and use backend URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("process.env.REACT_APP_API_URL/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
